f353083ddb92424085b586535e26b3d9cf559ae5
Andrew Stake <andrewms98@live.com>
2018-10-30 22:09:28 -0400

    replaced MyGdxGame with modified version of CanyonBunnyMain

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,29 +5,58 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL30;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	
+	private final static String TAG = MyGdxGame.class.getName();
+	private WorldController worldController;
+	private WorldRenderer worldRenderer;
+	private boolean paused;
 	
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {	
+		//Set libgdx log level to debug
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		 // Load assets
+		 Assets.instance.init(new AssetManager());
+		//Initialize controller and renderer
+		worldController = new WorldController();
+		worldRenderer = new WorldRenderer(worldController);
+		paused = false;
+		
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void resize(int width, int height) {
+		worldRenderer.resize(width,height);
+	}
+
+	public void render() {
+		if(!paused)
+		{
+			worldController.update(Gdx.graphics.getDeltaTime());
+		}
+		Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f, 0xed/255.0f, 0xff/255.0f);
+		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+		worldRenderer.render();
+		
 	}
 	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void pause() {
+		paused = true;
+	}
+
+	public void resume() {
+		Assets.instance.init(new AssetManager());
+		paused = false;
+	}
+
+	public void dispose() {
+		worldRenderer.dispose();
+		Assets.instance.dispose();
 	}
 }
