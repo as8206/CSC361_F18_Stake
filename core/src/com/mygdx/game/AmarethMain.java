cca52a3978ef0828e2b0538a419f1d29a06ad823
Unknown <andrewms98@live.com>
2018-12-11 18:46:16 -0500

    Build game over screen without fade

--- core/src/com/mygdx/game/AmarethMain.java
+++ core/src/com/mygdx/game/AmarethMain.java
@@ -1,6 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.mygdx.game.screens.GameOverScreen;
 import com.mygdx.game.screens.MenuScreen;
 import com.mygdx.game.utils.AudioManager;
 import com.mygdx.game.utils.GamePreferences;
@@ -23,6 +24,6 @@ public class AmarethMain extends Game
 		GamePreferences.instance.load();
 		
 		// Start game at menu screen
-		setScreen(new MenuScreen(this));
+		setScreen(new GameOverScreen(this,0));
 	}
 }
